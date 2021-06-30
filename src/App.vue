03c75693a16b45ae532fdffaaaf27739e56ffec7
lyc <22719973+l-yc@users.noreply.github.com>
2021-06-30 16:37:42 +0800

    add slide switching

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <main id="app" class="flex flex-col">
     <div id="toolbar">
-      <button class="button">Prev</button>
-      <button class="button">Next</button>
+      <button @click="prevSlide" class="btn btn-blue">Prev</button>
+      <button @click="nextSlide" class="btn btn-blue">Next</button>
+      <button @click="newSlide" class="btn btn-blue">New</button>
+      <p>{{ activeSlideIndex + 1 }} / {{ slides.length }}</p>
       <input v-model="ratio" type="text" />
     </div>
     <div id="workspace" class="flex-1 flex flex-row">
       <div id="editor-pane" class="flex-1">
         <h1>Editor</h1>
-        <hr>
+        <hr />
         <div ref="editor"></div>
       </div>
       <div id="preview-pane" class="flex-1">
         <h1>Preview</h1>
-        <hr>
+        <hr />
         <div :style="ratioStyle">
           <div id="preview" ref="preview" v-html="output"></div>
         </div>
@@ -35,33 +37,39 @@ export default defineComponent({
   components: {},
   data() {
     return {
+      cm: null,
       ratio: "4:3",
-      input: "",
+      slides: [""],
+      activeSlideIndex: 0,
     };
   },
   computed: {
     ratioStyle() {
-      let x = this.ratio.split(':')
-      let n = parseInt(x[1]), d = parseInt(x[0])
-      return `position: relative; width: 100%; padding-bottom: ${(n/d*100).toString()}%`
+      let x = this.ratio.split(":");
+      let n = parseInt(x[1]),
+        d = parseInt(x[0]);
+      return `position: relative; width: 100%; padding-bottom: ${(
+        (n / d) *
+        100
+      ).toString()}%`;
     },
     output() {
-      return DOMPurify.sanitize(marked(this.input));
+      return DOMPurify.sanitize(marked(this.slides[this.activeSlideIndex]));
     },
   },
 
   mounted() {
     let vm = this;
 
-    const view = new EditorView({
-      parent: <Element> this.$refs.editor,
+    this.cm = new EditorView({
+      parent: <Element>this.$refs.editor,
       state: EditorState.create({
         doc: "",
         extensions: [
           basicSetup,
           EditorView.updateListener.of((v: ViewUpdate) => {
             if (v.docChanged) {
-              this.input = v.state.doc.toString();
+              vm.updateCurrentSlide(v.state.doc.toString());
             }
           }),
         ],
@@ -70,12 +78,45 @@ export default defineComponent({
   },
 
   methods: {
-  }
+    prevSlide() {
+      if (this.activeSlideIndex > 0) {
+        this.loadSlide(this.activeSlideIndex-1);
+      }
+    },
+
+    nextSlide() {
+      if (this.activeSlideIndex < this.slides.length-1) {
+        this.loadSlide(this.activeSlideIndex+1);
+      }
+    },
+
+    newSlide() {
+      this.slides.push("");
+      this.loadSlide(this.slides.length - 1);
+    },
+
+    loadSlide(slideNo: number) {
+      this.activeSlideIndex = slideNo;
+      this.cm.dispatch({
+        changes: {
+          from: 0, 
+          to: this.cm.state.doc.length, 
+          insert: this.slides[this.activeSlideIndex]
+        },
+      });
+    },
+
+    updateCurrentSlide(content: string) {
+      this.slides[this.activeSlideIndex] = content;
+    },
+  },
 });
 </script>
 
 <style lang="postcss">
-html, body, #app {
+html,
+body,
+#app {
   height: 100%;
 }
 
@@ -86,5 +127,15 @@ html, body, #app {
   left: 0;
   width: 100%;
   height: 100%;
+}
+
+.btn {
+  @apply font-bold py-2 px-4 rounded;
+}
+.btn-blue {
+  @apply bg-blue-500 text-white;
+}
+.btn-blue:hover {
+  @apply bg-blue-700;
 }
 </style>
