f971387692d2fea104547eefdcd2b4b274a0ff74
lyc <22719973+l-yc@users.noreply.github.com>
2021-06-30 16:20:27 +0800

    add header styles and slide aspect ratio config

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,20 @@
     <div id="toolbar">
       <button class="button">Prev</button>
       <button class="button">Next</button>
+      <input v-model="ratio" type="text" />
     </div>
     <div id="workspace" class="flex-1 flex flex-row">
       <div id="editor-pane" class="flex-1">
         <h1>Editor</h1>
         <hr>
-        <!--<input v-model="input" type="text" />-->
         <div ref="editor"></div>
       </div>
       <div id="preview-pane" class="flex-1">
         <h1>Preview</h1>
         <hr>
-        <div id="preview" ref="preview" v-html="output"></div>
+        <div :style="ratioStyle">
+          <div id="preview" ref="preview" v-html="output"></div>
+        </div>
       </div>
     </div>
   </main>
@@ -33,10 +35,16 @@ export default defineComponent({
   components: {},
   data() {
     return {
+      ratio: "4:3",
       input: "",
     };
   },
   computed: {
+    ratioStyle() {
+      let x = this.ratio.split(':')
+      let n = parseInt(x[1]), d = parseInt(x[0])
+      return `position: relative; width: 100%; padding-bottom: ${(n/d*100).toString()}%`
+    },
     output() {
       return DOMPurify.sanitize(marked(this.input));
     },
@@ -60,11 +68,23 @@ export default defineComponent({
       }),
     });
   },
+
+  methods: {
+  }
 });
 </script>
 
 <style lang="postcss">
+html, body, #app {
+  height: 100%;
+}
+
 #preview {
   border: 2px solid black;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
