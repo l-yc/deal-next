79c40897694dc30a5564df46d758b157dbdf7490
lyc <22719973+l-yc@users.noreply.github.com>
2021-07-05 11:14:16 +0800

    refactor theme

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 
   import Modal from "./lib/Modal.svelte";
   import renderMarkdown from "./lib/Renderer";
+  import { theme, generateScopedStyle } from "./lib/Theme";
 
   import * as _ from "lodash";
 
@@ -21,7 +22,10 @@
   } from "@martinse/svelte-heroicons/dist/solid";
 
   // refs
-  let importedSlides: HTMLInputElement, editor: Element, preview: HTMLElement, previewWrapper: HTMLElement;
+  let importedSlides: HTMLInputElement,
+    editor: Element,
+    preview: HTMLElement,
+    previewWrapper: HTMLElement;
 
   // data
   let cm = null as EditorView | null;
@@ -29,52 +33,8 @@
   let slides = [{ content: "", notes: "" }];
   let activeSlideIndex = 0;
 
-  let themeDefault = `
-slide {
-  background-color: white;
-  padding: 1rem;
-}
-
-h1 {
-  font-size: 1.6rem;
-}
-
-li {
-  list-style: disc inside;
-}
-`;
-
-  let theme = {
-    default: themeDefault,
-  };
-
   $: activeTheme = theme.default;
   $: activeThemeOutput = generateScopedStyle(activeTheme, "#preview");
-
-  function generateScopedStyle(styles: string, scope: string): string {
-    let doc = document.implementation.createHTMLDocument(""),
-      styleElement = document.createElement("style");
-
-    styleElement.textContent = styles;
-    // the style will only be parsed once it is added to a document
-    doc.body.appendChild(styleElement);
-
-    let newStyle = "";
-    for (let rule of styleElement.sheet.cssRules) {
-      if (rule instanceof CSSStyleRule) {
-        if (rule.selectorText == "slide") {
-          rule.selectorText = `${scope}`;
-        } else {
-          rule.selectorText = `${scope} ${rule.selectorText}`;
-        }
-      }
-      newStyle += rule.cssText;
-    }
-
-    console.log("loading theme", newStyle);
-    // svelte-preprocess is trying to parse the style tag, so we split it up ???
-    return `<st` + `yle data-theme>${newStyle}</st` + `yle>`;
-  }
 
   let exportModalVisible = false;
   let importModalVisible = false;
@@ -156,8 +116,7 @@ li {
           }),
           EditorView.domEventHandlers({
             paste(event: ClipboardEvent, v: EditorView) {
-              let items = (
-                // @ts-ignore originalEvent for newer chrome version
+              let items = ( // @ts-ignore originalEvent for newer chrome version
                 event.clipboardData || event.originalEvent.clipboardData
               )?.items;
               for (let index in items) {
@@ -267,9 +226,9 @@ li {
       const prtHtml = slides
         .map(
           (s) =>
-            `<div class="slide border-2 border-black" style="${ratioStyle}">${DOMPurify.sanitize(
-              marked(s.content)
-            )}</div>`
+            `<div class="slide border-2 border-black" style="${ratioStyle}">
+              ${renderMarkdown(s.content)}
+            </div>`
         )
         .join("<footer></footer>");
 
---
+++ src/lib/Theme.ts
@@ -0,0 +1,48 @@
+const themeDefault = `
+slide {
+  background-color: white;
+  padding: 1rem;
+}
+
+h1 {
+  font-size: 1.6rem;
+}
+
+li {
+  list-style: disc inside;
+}
+`;
+
+const theme = {
+  default: themeDefault,
+};
+
+function generateScopedStyle(styles: string, scope: string): string {
+  let doc = document.implementation.createHTMLDocument(""),
+    styleElement = document.createElement("style");
+
+  styleElement.textContent = styles;
+  // the style will only be parsed once it is added to a document
+  doc.body.appendChild(styleElement);
+
+  let newStyle = "";
+  for (let rule of styleElement.sheet.cssRules) {
+    if (rule instanceof CSSStyleRule) {
+      if (rule.selectorText == "slide") {
+        rule.selectorText = `${scope}`;
+      } else {
+        rule.selectorText = `${scope} ${rule.selectorText}`;
+      }
+    }
+    newStyle += rule.cssText;
+  }
+
+  console.log("loading theme", newStyle);
+  // svelte-preprocess is trying to parse the style tag, so we split it up ???
+  return `<st` + `yle data-theme>${newStyle}</st` + `yle>`;
+}
+
+export {
+  theme,
+  generateScopedStyle,
+}
