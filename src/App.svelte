4f9986c6d3afe6b3ea4a13913dd555306136d7fe
lyc <22719973+l-yc@users.noreply.github.com>
2021-07-16 09:56:12 +0800

    refactor export slide functionality

--- src/App.svelte
+++ src/App.svelte
@@ -6,13 +6,12 @@
   import { theme, generateScopedStyle } from "./lib/Theme";
   import type { Keybind } from "./lib/Keybindings";
   import { registerDocumentKeybindings } from "./lib/Keybindings";
+  import { exportSlides_ } from "./lib/IO";
 
   import * as _ from "lodash";
 
   import { basicSetup, EditorState, EditorView } from "@codemirror/basic-setup";
   import { ViewUpdate, keymap } from "@codemirror/view";
-
-  import FileSaver from "file-saver";
 
   import Heroicon from "@martinse/svelte-heroicons";
   import {
@@ -323,83 +322,7 @@
   }
 
   function exportSlides(typ: string) {
-    if (typ === "deal") {
-      let exportData = {
-        settings: settings,
-        slides: slides,
-      };
-      let blob = new Blob([JSON.stringify(exportData)], {
-        type: "text/plain;charset=utf-8",
-      });
-      FileSaver.saveAs(blob, title + ".deal");
-    } else if (typ == "pdf") {
-      const prtHtml = slides
-        .map(
-          (s, i) =>
-            `<div class="slide border-2 border-black" style="${ratioStyle}">
-              ${renderMarkdown(s.content)}
-              <div class="slide-number" style="position: absolute; bottom: .2rem; right: .4rem;">${i+1}</div>
-            </div>`
-        )
-        .join("<footer></footer>");
-
-      let stylesHtml = "";
-      for (const node of [
-        ...Array.from(
-          document.querySelectorAll('link[rel="stylesheet"], style')
-        ),
-      ])
-        if (node instanceof HTMLElement && !("theme" in node.dataset)) {
-          // we will add the theme css ourselves
-          stylesHtml += node.outerHTML;
-        }
-      stylesHtml += generateScopedStyle(activeTheme, ".slide");
-
-      // add print css using pagedjs
-      let x = settings.ratio.split(":");
-      let n = parseInt(x[1]),
-        d = parseInt(x[0]);
-      let w = "30rem",
-        h = (30 * n) / d + "rem";
-      console.log(n / d);
-      stylesHtml +=
-        `<st` +
-        `yle>
-        @page {
-          margin: 0mm 0mm;
-          size: ${w} ${h};
-        }
-      </st` +
-        `yle>`;
-      stylesHtml +=
-        `<scr` +
-        `ipt src="https://unpkg.com/pagedjs/dist/paged.polyfill.js"></scr` +
-        `ipt>`;
-
-      const WinPrint = <Window>(
-        window.open(
-          "",
-          "",
-          "left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0"
-        )
-      );
-
-      WinPrint.document.write(`<!DOCTYPE html>
-        <html>
-          <head>
-            ${stylesHtml}
-          </head>
-          <body>
-            ${prtHtml}
-          </body>
-        </html>`);
-
-      WinPrint.document.close();
-      WinPrint.focus();
-    } else {
-      console.log(typ);
-      alert("Error: unimplemented!");
-    }
+    exportSlides_(typ, title, ratioStyle, activeTheme, settings, slides);
   }
 
   function showImportSlidesModal() {
---
+++ src/lib/IO.ts
@@ -0,0 +1,86 @@
+import { generateScopedStyle } from "./lib/Theme";
+import renderMarkdown from "./Renderer";
+
+import FileSaver from "file-saver";
+
+function exportSlides_(typ: string, title: string, ratioStyle: string, activeTheme: string, settings: object, slides: any) {
+  if (typ === "deal") {
+    let exportData = {
+      settings: settings,
+      slides: slides,
+    };
+    let blob = new Blob([JSON.stringify(exportData)], {
+      type: "text/plain;charset=utf-8",
+    });
+    FileSaver.saveAs(blob, title + ".deal");
+  } else if (typ == "pdf") {
+    const prtHtml = slides
+      .map(
+        (s, i) =>
+          `<div class="slide border-2 border-black" style="${ratioStyle}">
+              ${renderMarkdown(s.content)}
+              <div class="slide-number" style="position: absolute; bottom: .2rem; right: .4rem;">${i + 1}</div>
+            </div>`
+      )
+      .join("<footer></footer>");
+
+    let stylesHtml = "";
+    for (const node of [
+      ...Array.from(
+        document.querySelectorAll('link[rel="stylesheet"], style')
+      ),
+    ])
+      if (node instanceof HTMLElement && !("theme" in node.dataset)) {
+        // we will add the theme css ourselves
+        stylesHtml += node.outerHTML;
+      }
+    stylesHtml += generateScopedStyle(activeTheme, ".slide");
+
+    // add print css using pagedjs
+    let x = settings.ratio.split(":");
+    let n = parseInt(x[1]),
+      d = parseInt(x[0]);
+    let w = "30rem",
+      h = (30 * n) / d + "rem";
+    console.log(n / d);
+    stylesHtml +=
+      `<st` +
+      `yle>
+        @page {
+          margin: 0mm 0mm;
+          size: ${w} ${h};
+        }
+      </st` +
+      `yle>`;
+    stylesHtml +=
+      `<scr` +
+      `ipt src="https://unpkg.com/pagedjs/dist/paged.polyfill.js"></scr` +
+      `ipt>`;
+
+    const WinPrint = <Window>(
+      window.open(
+        "",
+        "",
+        "left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0"
+      )
+    );
+
+    WinPrint.document.write(`<!DOCTYPE html>
+        <html>
+          <head>
+            ${stylesHtml}
+          </head>
+          <body>
+            ${prtHtml}
+          </body>
+        </html>`);
+
+    WinPrint.document.close();
+    WinPrint.focus();
+  } else {
+    console.log(typ);
+    alert("Error: unimplemented!");
+  }
+}
+
+export {exportSlides_};
